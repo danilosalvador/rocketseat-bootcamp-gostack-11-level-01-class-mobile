72bc59f53afba736b205b2619044d84c9714718a
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-27 00:07:49 -0300

    features:Criando novos projetos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView, FlatList, Text, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -19,18 +19,41 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${projects.length}`,
+      owner: 'Danilo Salvador',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor='#476b9e'/>
 
       <SafeAreaView
         style={styles.container}>
+
+        <Text style={styles.hello}>Hello GoStack 🚀</Text>
+
         <FlatList
           data={projects}
           keyExtractor={project => project.id}
           renderItem={({ item:project }) => (
-            <Text style={styles.title}>{project.title}</Text>
+            <Text style={styles.title}>{project.title} ({project.owner})</Text>
           )}/>
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+
       </SafeAreaView>
 
       {/* <View style={styles.container}>
@@ -49,8 +72,26 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
     // alignItems: 'center',
   },
+  hello: {
+    margin: 10,
+    color: '#fff',
+    fontSize: 44,
+    textAlign: 'center'
+  },
   title: {
     color: '#fff',
     fontSize: 18,
   },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
